8441998eb381226ff928e5fc7c24c7d78ffac208
ruin.3899 <ruin.38999@gmail.com>
2025-09-14 21:54:29 +0900

    javascript化

---
+++ index.js
@@ -0,0 +1,32 @@
+function makeNews(newsArray, num) {
+    const newsElement = document.createElement('article');
+    newsElement.className = 'news';
+    const newsHeading = document.createElement('h2');
+    newsHeading.textContent = newsArray[num][0];
+    const newsParagraph = document.createElement('p');
+    newsParagraph.textContent = newsArray[num][1];
+    const newsDate = document.createElement('p');
+    newsDate.className = 'date';
+    newsDate.textContent = newsArray[num][2];
+    newsElement.appendChild(newsHeading);
+    newsElement.appendChild(newsParagraph);
+    newsElement.appendChild(newsDate);
+    mainElement.appendChild(newsElement);
+}
+
+const parentElement = document.createElement('main');
+const mainElement = document.createElement('section');
+mainElement.id = 'main';
+const mainHeader = document.createElement('h1');
+mainHeader.textContent = 'ニュース';
+mainElement.appendChild(mainHeader);
+/* ニュースの内容 見出し、本文、日付 */
+const newsArray = [
+    ['ホームページシステム変更', 'ホームページをHTMLに直接書く形からJavaScriptから要素を作る形に変更しました！作業簡略化などのメリットがあります。なお、見た目は全く変わっていません。', '2025/09/14'],
+    ['ホームページ開設しました！', '作品を紹介する用のホームページを開設しました。作業風景等のブログも公開しようと思います。更新をお楽しみに！', '2025/09/12']
+    ];
+for (let i = 0; i < newsArray.length; i++) {
+    makeNews(newsArray, i);
+}
+parentElement.appendChild(mainElement);
+document.body.appendChild(parentElement);
--- script.js
+++ script.js
@@ -1,1 +1,102 @@
+const headerElement = document.createElement('header');
+const headerImg = document.createElement('img');
+headerImg.src = 'sitelogo.png';
+headerImg.alt = 'るいんの遺跡';
+headerImg.style.cursor = 'pointer';
+headerImg.onclick = function () {
+    location.href = 'index.html';
+};
+headerElement.appendChild(headerImg);
 
+const headerParagraph = document.createElement('p');
+headerParagraph.textContent = '奏色瑠音のホームページ';
+headerElement.appendChild(headerParagraph);
+
+const navElement = document.createElement('nav');
+const navList = document.createElement('ul');
+navList.innerHTML = `
+    <li><a href="index.html">TOP</a></li>
+    <li><a href="works.html">WORKS</a></li>
+    <li><a href="about.html">ABOUT</a></li>
+    <li style="border-right: none;"><a href="https://forms.gle/2p77JCtYXe8Rsdzz5">CONTACT</a></li>
+    `;
+navElement.appendChild(navList);
+headerElement.appendChild(navElement);
+
+document.body.insertBefore(headerElement, document.body.firstChild);
+
+const leftElement = document.createElement('section');
+leftElement.id = 'left';
+
+const introElement = document.createElement('div');
+introElement.id = 'intro';
+const introHeading = document.createElement('h2');
+introHeading.textContent = '管理人';
+const introImg = document.createElement('img');
+introImg.src = 'ruinicon.png';
+introImg.alt = 'プロフィール画像';
+introImg.style.width = '90%';
+introParagraph = document.createElement('div');
+introParagraph.innerHTML = `<p>名前：<ruby>奏色瑠音<rt>かないろるいん</rt></ruby><br>
+とにかくゲームが好きな人。カドゲ、音ゲなどなどいろんなゲームをやってる。動画編集もやってて、YouTubeに投稿してたりする。一応ゲーム作るのが本業。`
+introElement.appendChild(introHeading);
+introElement.appendChild(introImg);
+introElement.appendChild(introParagraph);
+leftElement.appendChild(introElement);
+
+const twitterElement = document.createElement('div');
+twitterElement.id = 'twitter';
+const twitterHeading = document.createElement('h2');
+twitterHeading.textContent = 'X';
+const twitterAnchor = document.createElement('a');
+twitterAnchor.setAttribute('class', 'twitter-timeline');
+twitterAnchor.textContent = 'Tweets by ruin3899';
+twitterAnchor.setAttribute('href', 'https://twitter.com/ruin3899?ref_src=twsrc%5Etfw');
+const twitterLink = document.createElement('script');
+twitterLink.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+twitterLink.setAttribute('charset', 'utf-8');
+twitterLink.setAttribute('async', '');
+twitterElement.appendChild(twitterHeading);
+twitterElement.appendChild(twitterAnchor);
+twitterElement.appendChild(twitterLink);
+leftElement.appendChild(twitterElement);
+
+const mainContent = document.getElementById('main');
+mainContent.parentNode.insertBefore(leftElement, mainContent);
+
+const rightElement = document.createElement('section');
+rightElement.id = 'right';
+
+const calendarElement = document.createElement('div');
+calendarElement.id = 'calendar';
+const calendarHeading = document.createElement('h2');
+calendarHeading.textContent = 'カレンダー';
+const calendarIframe = document.createElement('iframe');
+calendarIframe.src = 'https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FTokyo&showPrint=0&showTitle=0&src=cnVpbi4zODk5OUBnbWFpbC5jb20&src=amEuamFwYW5lc2UjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039be5&color=%230b8043';
+calendarIframe.style.border = 'solid 1px #777';
+calendarIframe.width = '90%';
+calendarIframe.height = '600';
+calendarIframe.setAttribute('frameborder', '0');
+calendarIframe.setAttribute('scrolling', 'no');
+calendarElement.appendChild(calendarHeading);
+calendarElement.appendChild(calendarIframe);
+rightElement.appendChild(calendarElement);
+
+const linksElement = document.createElement('div');
+linksElement.id = 'links';
+const linksHeading = document.createElement('h2');
+linksHeading.textContent = 'リンク';
+const linksList = document.createElement('ul');
+linksList.innerHTML = `
+    <li><a href="https://twitter.com/ruin3899" target="_blank" rel="noopener">X</a></li>
+    <li><a href="http://www.youtube.com/@ruin3899" target="_blank" rel="noopener">YouTube</a></li>
+    `;
+linksElement.appendChild(linksHeading);
+linksElement.appendChild(linksList);
+rightElement.appendChild(linksElement);
+
+mainContent.parentNode.insertBefore(rightElement, mainContent.nextSibling);
+
+const footerElement = document.createElement('footer');
+footerElement.innerHTML = `<p>Copyright &copy; 2025 奏色瑠音 All Rights Reserved.</p>`
+document.body.appendChild(footerElement);
