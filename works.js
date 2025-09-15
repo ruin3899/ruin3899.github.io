35d7d8decb3898540793306b6b33a260a75e18b5
ruin.3899 <ruin.38999@gmail.com>
2025-09-15 18:05:19 +0900

    CSSの調整

---
+++ works.js
@@ -0,0 +1,7 @@
+const mainElement = document.createElement('main');
+const worksHeading = document.createElement('h1');
+worksHeading.textContent = '作品集';
+mainElement.appendChild(worksHeading);
+
+
+document.body.appendChild(mainElement);
