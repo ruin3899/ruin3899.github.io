f040546c279f190ac3eeabb9c013f79cd9bf0d74
ruin3899.mac <{ruin.38999@gmail.com}>
2026-01-19 19:22:17 +0900

    enhance seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   // 自分のGitHub PagesのURL
   site: 'https://ruin3899.github.io',
+
+  integrations: [sitemap()],
 });
